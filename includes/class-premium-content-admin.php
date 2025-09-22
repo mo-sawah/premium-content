8a3eeed0eea494be3d99adf6f71d47972537814b
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-23 01:50:17 +0300

    Update version to 1.3.1 and sanitize email input before saving

--- includes/class-premium-content-admin.php
+++ includes/class-premium-content-admin.php
@@ -426,7 +426,8 @@ class Premium_Content_Admin {
 
             foreach ($text_fields as $field => $default) {
                 if (isset($_POST[$field])) {
-                    update_option('premium_content_' . $field, wp_kses_post($_POST[$field]));
+                    $value = stripslashes_deep($_POST[$field]);
+                    update_option('premium_content_' . $field, wp_kses_post($value));
                 }
             }
 
--- premium-content.php
+++ premium-content.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Premium Content
  * Description: Truncates premium articles and prompts for an email to continue reading.
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: Mohamed Sawah
  */
 
