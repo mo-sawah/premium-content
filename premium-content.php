6abbf31f7a2cb9b2b7724d81dd0f34aa950be671
Mohamed Sawah <mosawah@sawahsolutions.com>
2025-09-23 12:33:18 +0300

    Bump version to 1.4.1

--- premium-content.php
+++ premium-content.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Premium Content
  * Description: Truncates premium articles and prompts for an email to continue reading.
- * Version: 1.4.0
+ * Version: 1.4.1
  * Author: Mohamed Sawah
  */
 
